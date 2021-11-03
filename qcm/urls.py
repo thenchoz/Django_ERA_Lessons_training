a25dbaa304c5db26d0fda4a77181593216f95197
Tristan <tristan.henchoz@gmail.com>
2021-11-03 21:57:22 +0100

    correct html

--- qcm/urls.py
+++ qcm/urls.py
@@ -10,7 +10,19 @@ app_name = "qcm"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("branch-<int:pk>/", views.DetailBranchView.as_view(), name="detail"),
-    path("branch-<int:branch_id>/question-<int:pk>/", views.DetailQuestionView.as_view(), name="detail_question"),
-    path("branch-<int:branch_id>/question-<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    path("branch-<int:branch_id>/question-<int:question_id>/answer/", views.answer, name="answer"),
+    path(
+        "branch-<int:branch_id>/question-<int:pk>/",
+        views.DetailQuestionView.as_view(),
+        name="detail_question",
+    ),
+    path(
+        "branch-<int:branch_id>/question-<int:pk>/results/",
+        views.ResultsView.as_view(),
+        name="results",
+    ),
+    path(
+        "branch-<int:branch_id>/question-<int:question_id>/answer/",
+        views.answer,
+        name="answer",
+    ),
 ]
