de4a1e3289a7e3556b8d4b315469860bdb6c9f93
THenchoz <tristan.henchoz@gmail.com>
2021-11-10 10:03:25 +0100

    change in settings for production setup

--- era/settings.py
+++ era/settings.py
@@ -10,16 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-
 from . import credentials
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = credentials.BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = credentials.SECRET_KEY
@@ -27,7 +24,7 @@ SECRET_KEY = credentials.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = credentials.DEBUG
 
-ALLOWED_HOSTS = []  # type: ignore
+ALLOWED_HOSTS = credentials.ALLOWED_HOSTS
 
 
 # Application definition
@@ -52,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "era.urls"
+ROOT_URLCONF = credentials.ROOT_URLCONF
 
 TEMPLATES = [
     {
@@ -70,59 +67,36 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "era.wsgi.application"
+WSGI_APPLICATION = credentials.WSGI_APPLICATION
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
+DATABASES = credentials.DATABASES
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
+AUTH_PASSWORD_VALIDATORS = credentials.AUTH_PASSWORD_VALIDATORS
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = credentials.LANGUAGE_CODE
 
-TIME_ZONE = "UTC"
+TIME_ZONE = credentials.TIME_ZONE
 
-USE_I18N = True
+USE_I18N = credentials.USE_I18N
 
-USE_L10N = True
+USE_L10N = credentials.USE_L10N
 
-USE_TZ = True
+USE_TZ = credentials.USE_TZ
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = credentials.STATIC_URL
+STATIC_ROOT = credentials.STATIC_ROOT
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = credentials.DEFAULT_AUTO_FIELD
