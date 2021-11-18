82407d5d5c26258bc1919ddd693f2e6654a7667e
THenchoz <tristan.henchoz@gmail.com>
2021-11-18 22:33:32 +0100

    start new app user_data

---
+++ user_data/admin.py
@@ -0,0 +1,19 @@
+"""
+Django admin setup for user_data app.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/ref/contrib/admin/actions/
+"""
+
+from django.contrib import admin
+
+
+class UserDataAdminSite(admin.AdminSite):
+    """class to adapte the admin site"""
+
+    site_header = "ERA User Data Administration"
+    site_title = "User Data"
+    index_title = "ERA Administration"
+
+
+user_data_admin_site = UserDataAdminSite(name="user_data_admin")
---
+++ user_data/models.py
@@ -0,0 +1,10 @@
+"""
+Django models for user_data app.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/topics/db/models/
+"""
+
+# from django.db import models
+
+# Create your models here.
